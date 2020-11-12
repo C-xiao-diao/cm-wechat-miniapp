4b02eb8ab46eb0389da39fe5e4244875583dba1c
lmn_741 <15111205994@163.com>
2020-11-12 11:10:27 +0800

    新增热榜界面

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,7 +9,7 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     //---------start ----------
-    list: [1,1,1,1,1,1,1,1,,1,1,1,1]
+    list: [1, 1, 1, 1, 1, 1, 1, 1, , 1, 1, 1, 1]
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
@@ -17,7 +17,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -39,7 +39,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
@@ -49,8 +49,12 @@ Page({
   },
 
   // ---------------------- start ------------------------------
-  onShareAppMessage: function(e){
+  onShareAppMessage: function (e) {
 
   },
+  // 点赞
+  like: function(){
+    
+  }
   // ----------------------  end --------------------------------
 })
