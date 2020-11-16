c1b267cd67a1e2602bb848d6f73d0b79bf0ac9b3
lmn_741 <15111205994@163.com>
2020-11-16 17:39:50 +0800

    删除console.log

--- pages/follow/follow.js
+++ pages/follow/follow.js
@@ -33,10 +33,10 @@ Page({
       sizeType: ['original', 'compressed'],
       sourceType: ['album', 'camera'],
       success(res) {
-        console.log("111111111111111111111111111111", res)
         // tempFilePath可以作为img标签的src属性显示图片
         const tempFilePaths = res.tempFilePaths;
         //上传至服务器
+        wx.showLoading({ title: '正在上传...'});
         wx.uploadFile({
           url: config.uploadUrl,
           filePath: tempFilePaths[0],
@@ -51,18 +51,17 @@ Page({
             let json = res.data;
             let resData = JSON.parse(json)
             if(_.get(resData, 'code') ===200){
-              console.log(resData ,'resDataresDataresDataresData')
               let fileName = _.get(resData, 'data.fileName');
               let file = fileName[0];
               let picture = _this.data.picture;
               let pics = _.uniq(_.concat(picture, fileName));
-              console.log(pics, 'mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm')
               _this.setData({picture: pics});
             } else {
 
             }
-            //do something
-            console.log(res, 4444444444444444444444444444444444);
+          },
+          complete: function(){
+            wx.hideLoading();
           }
         })
       }
@@ -91,7 +90,6 @@ Page({
       content,
       picture
     }
-    console.log(data, 'vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv')
     http.post({
       cmd,
       data,
@@ -103,13 +101,10 @@ Page({
               wx.navigateBack({
                 delta: 0,
               })
-              // wx.reLaunch({
-              //   url: '/pages/forumList/forumList?themeId=' + themeId
-              // })
+          
             },         
           })
         }
-        console.log(res, 999999999999999999999999999999);
       }
     })
   }
