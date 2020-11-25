2328ebc44df8f8632b8876adee2a30c4429c305f
Zhong Yitian <363778748@qq.com>
2020-11-25 16:54:41 +0800

    方法完善

--- component/more-button/more-button.js
+++ component/more-button/more-button.js
@@ -1,24 +1,20 @@
 Component({
-    properties: {
-        showBtn: {
-            type: Boolean,
-            value: false
-        }
-    },
     data: {
         showList: false
     },
     lifetimes: {
         // 在组件实例进入页面节点树时执行
         attached: function() {
-            let showBtn = this.properties.showBtn;
-            console.log(showBtn,9999999999)
-            this.setData({ showList: showBtn }) 
+            
         }
     },
     methods: {
         showListOrNot: function(){
-            let showList = !this.data.showList;
+            this.showListFn(false);
+        },
+        showListFn: function(fromParent){
+            let showList;
+            fromParent ? showList = false : showList = !this.data.showList;
             this.setData({showList});
         }
     }
