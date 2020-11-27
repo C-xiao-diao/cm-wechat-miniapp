1ac06a61bc8f47e4a47f48899106b02c0e625a80
lmn_741 <15111205994@163.com>
2020-11-27 11:33:40 +0800

    修正首页BUG

--- pages/index/index.js
+++ pages/index/index.js
@@ -258,9 +258,16 @@ Page({
   },
   //前往注册界面
   navToRegister: function () {
-    wx.navigateTo({
-      url: '/pages/registerOne/registerOne',
-    })
+    let step = app.globalData.step;
+    if(step === 0){
+      //注册已完成，实际上不会看到注册按钮
+    } else if(step === 1){
+      wx.navigateTo({ url: '/pages/registerOne/registerOne'})
+    } else if(step === 2){
+      wx.navigateTo({ url: '/pages/registerTwo/registerTwo'})
+    } else {
+      wx.navigateTo({ url: '/pages/registerOne/registerOne'})
+    }
   },
   //前往个人中心界面
   navToMemberCenter: function () {
