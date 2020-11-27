083c800cdae3f3c401dea6db5b13185a213f3c8b
Zhong Yitian <363778748@qq.com>
2020-11-27 16:58:26 +0800

    邀请页联调

--- pages/invite/invite.js
+++ pages/invite/invite.js
@@ -1,7 +1,24 @@
+import "./../../utils/fix";
+import _ from "./../../utils/lodash";
+import config from "./../../configs/config"
+import { http } from "./../../utils/util";
+
+//获取应用实例
+const app = getApp()
 
 Page({
     data: {
-
+        userInfo: '',
+        inviteCount: 0,
+        inviteReword: 0,
+        headimgUrlList: [''],
+    },
+    onLoad: function(){
+        let userInfo = app.globalData.userInfo;
+        if(userInfo){
+            this.setData({userInfo});
+        }
+        this.getInviter();
     },
     onShareAppMessage: function (e) {
         // let timestamp = Date.parse(new Date());
@@ -12,9 +29,28 @@ Page({
         //     imageUrl: ''
         // }
     },
+    //查看音符规则
     toRules: function(){
         wx.navigateTo({
             url: '../rules/rules'
         });
+    },
+    //获取受邀人列表
+    getInviter: function(){
+        let cmd = "/auth/cWechat/inviter";
+        let data = { userId: app.globalData.userId }
+        http.get({
+            cmd,
+            data:data,
+            success: res => {
+                if (_.get(res, 'data.code') === 200 && !_.isEmpty(_.get(res, 'data.data'))) {
+                    let resData = _.get(res, 'data.data');
+                    let inviteCount = resData.count;
+                    let inviteReword = inviteCount * 200;
+                    let headimgUrlList = resData.list;
+                    this.setData({ inviteCount, inviteReword,headimgUrlList });
+                }
+            }
+        })
     }
 })
