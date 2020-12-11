7fa32b6c60c0fe817d6f08e3a89df8f8a1bed923
lmn_741 <15111205994@163.com>
2020-12-11 17:31:57 +0800

    新增三个界面

---
+++ pages/myFans/myFans.js
@@ -0,0 +1,14 @@
+//registerOne.js
+import { http,isJson } from "./../../utils/util";
+import config from "./../../configs/config"
+import "./../../utils/fix";
+import _ from "./../../utils/lodash"
+
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+
+  },
+}) 
