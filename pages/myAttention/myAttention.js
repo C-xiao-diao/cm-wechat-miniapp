0becbf86c66785e92acd1a2748b899a5640fe861
lmn_741 <15111205994@163.com>
2020-12-14 17:09:46 +0800

    对接部分接口

--- pages/myAttention/myAttention.js
+++ pages/myAttention/myAttention.js
@@ -9,6 +9,30 @@ const app = getApp()
 
 Page({
   data: {
-
+    limit: 10,
+    page: 0,
+    list: [],
   },
+  onLoad: function(option){
+    this.getMyAttentionList();
+  },
+  //加载我的关注列表
+  //params {boolean} isMore  是否加载更多
+  getMyAttentionList: function(isMore){
+    let cmd = "/auth/relation/myRelationList";
+    let { limit, page } = this.data;
+    let studentId = app.globalData.studentId;
+    let data ={ limit, page, studentId };
+    http.get({
+      cmd,
+      data,
+      success: res =>{
+        if(_.get(res, 'data.code') == 200){
+          let list = _.get(res, 'data.data.list');
+          this.setData({ list });
+        }
+        console.log(res,'==================================')
+      }
+    })
+  }
 }) 
--- pages/myFans/myFans.js
+++ pages/myFans/myFans.js
@@ -1,5 +1,5 @@
 //registerOne.js
-import { http,isJson } from "./../../utils/util";
+import { http, isJson } from "./../../utils/util";
 import config from "./../../configs/config"
 import "./../../utils/fix";
 import _ from "./../../utils/lodash"
@@ -9,6 +9,41 @@ const app = getApp()
 
 Page({
   data: {
-
+    limit: 10,
+    page: 0,
+    list: [],
   },
+  onLoad: function (option) {
+    this.getMyFansList();
+  },
+  //加载我的粉丝列表
+  //params {boolean} isMore  是否加载更多
+  getMyFansList: function (isMore) {
+    let cmd = "/auth/relation/myFansList";
+    let { limit, page } = this.data;
+    let studentId = app.globalData.studentId;
+    let data = { limit, page, studentId };
+    http.get({
+      cmd,
+      data,
+      success: res => {
+        if (_.get(res, 'data.code') == 200) {
+          let list = _.get(res, 'data.data.list');
+          this.setData({ list });
+        } else if (_.get(res, 'data.code') == 107) {
+          wx.showModal({
+            title: '提示',
+            content: _.get(res, 'data.msg') || '暂无数据',
+            success(res) {
+              if (res.confirm) {
+                wx.navigateBack({ delta: 0, })
+              } else if (res.cancel) {
+                wx.navigateBack({ delta: 0, })
+              }
+            }
+          })
+        }
+      }
+    })
+  }
 }) 
